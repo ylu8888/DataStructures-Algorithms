2df83e83f70f1806d0f8b74da3deee679ac953a4
ylu <123523291+ylu8888@users.noreply.github.com>
2023-07-02 19:07:20 -0400

    rename

---
+++ linkedlists/reverseLinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+class reverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+
+      // Move three pointers, current, head, and prev. Keep swapping pointers till you reach the end, return prev
+        while(head != null){
+            current = head.next;
+            head.next = prev;
+            prev = head;
+            head = current;
+        }
+
+        return prev;
+    }
+}
