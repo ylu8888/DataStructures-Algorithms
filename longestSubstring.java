3ee091f93d9f786b80a265e32a0e0be2dfc74801
ylu <123523291+ylu8888@users.noreply.github.com>
2023-07-02 19:18:43 -0400

    Update and rename validSudoku.java to longestSubstring.java

---
+++ longestSubstring.java
@@ -0,0 +1,22 @@
+class longestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int start = 0;
+        int end = 0;
+        int max = 0;
+
+        while(end < s.length()){
+            if(set.contains(s.charAt(end))){
+                set.remove(s.charAt(start));
+                start++;
+            }
+            else{
+                set.add(s.charAt(end));
+                end++;
+                max = Math.max(max, end - start);
+            }
+        }
+
+        return max;
+    }
+}
