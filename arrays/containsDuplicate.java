e471c0a3be7f721516cb6d5b2361b5bcf061bd65
ylu <123523291+ylu8888@users.noreply.github.com>
2023-07-03 22:31:38 -0400

    Create containsDuplicate.java

---
+++ arrays/containsDuplicate.java
@@ -0,0 +1,15 @@
+class containsDuplicate{
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            if(map.containsKey(nums[i])){
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
