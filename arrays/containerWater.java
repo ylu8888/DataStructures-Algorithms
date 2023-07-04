59594125f30a2cea4924be514c0df9c65207219b
ylu <123523291+ylu8888@users.noreply.github.com>
2023-07-03 22:35:49 -0400

    Create containerWater.java

---
+++ arrays/containerWater.java
@@ -0,0 +1,27 @@
+class containerWater {
+    public int maxArea(int[] height) {
+        int max = Integer.MIN_VALUE;
+        int left = 0;
+        int right = height.length - 1;
+
+        while(left < right){
+            int length = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = length * width;
+            
+            max = Math.max(area, max);
+
+            if(height[left] > height[right]){
+                right--;
+            }
+            else if(height[left] < height[right]){
+                left++;
+            }
+            else{
+                left++;
+            }
+        }
+
+        return max;
+    }
+}
