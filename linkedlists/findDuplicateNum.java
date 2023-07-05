b17334271882620451974b746c87ae7bf3d490a7
ylu <123523291+ylu8888@users.noreply.github.com>
2023-07-04 23:18:08 -0400

    Create findDuplicateNum.java

---
+++ linkedlists/findDuplicateNum.java
@@ -0,0 +1,16 @@
+class findDuplicateNum {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i])){
+                return nums[i];
+            }
+            else{
+                set.add(nums[i]);
+            }
+        }
+
+        return -1;
+    }
+}
