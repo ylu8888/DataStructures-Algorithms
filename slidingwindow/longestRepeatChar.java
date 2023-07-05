8265d6856e2871bfd3cb16bbdfd5637d62170c1f
ylu <123523291+ylu8888@users.noreply.github.com>
2023-07-04 23:15:55 -0400

    Create longestRepeatChar.java

---
+++ slidingwindow/longestRepeatChar.java
@@ -0,0 +1,43 @@
+class longestRepeatChar{
+    public int characterReplacement(String s, int k) {
+        HashSet<Character> map = new HashSet<>();
+        int max = 1;
+        int count = 0;
+        int temp = k;
+
+        for(int i = 0; i < s.length(); i++){
+            map = new HashSet<>();
+            count = 0;
+            map.add(s.charAt(i));
+            k = temp;
+           
+            for(int j=i; j < s.length(); j++){
+                if(map.contains(s.charAt(j))){
+                    count++;
+                    if(count > max){
+                        max = count;
+                    }
+                }
+                else{ 
+                  if(k != 0){
+                    count++;
+                    if(count > max){
+                    max = count;
+                    }
+                    k--;
+                    }
+                  else{
+                    break;
+                  }
+                    
+                    
+                }
+            }
+        }
+    
+    
+        
+
+        return max;
+    }
+}
