3ac28861b09026dca9ff7427ad7657330c28861b
ylu <123523291+ylu8888@users.noreply.github.com>
2023-07-04 23:17:07 -0400

    Create reorderList.java

---
+++ linkedlists/reorderList.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class reorderList {
+    public void reorderList(ListNode head) {
+        List<ListNode> arr = new ArrayList<>();
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        ListNode counter = head;
+        int count = 0;
+        while(counter != null){
+            counter = counter.next;
+            count++;
+        }
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if(count % 2 == 0){
+            while(slow != null){
+            arr.add(slow);
+            slow = slow.next;
+            }
+        }
+        else{
+            while(slow.next != null){
+            slow = slow.next;
+            arr.add(slow);
+            }
+        }
+        
+        int i = arr.size() - 1;
+        ListNode current = head;
+        ListNode original = head;
+        while(i >= 0){
+            original = original.next;
+            current.next = arr.get(i);
+            current = current.next;
+            current.next = original;
+            current = original;
+            i--;
+        }
+
+        current.next = null;
+
+    }
+}
