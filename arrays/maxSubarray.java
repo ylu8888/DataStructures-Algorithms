1a0a4d345b792752394cc10de57837593e062e92
ylu <123523291+ylu8888@users.noreply.github.com>
2023-07-07 04:11:50 -0400

    Create maxSubarray.java

---
+++ arrays/maxSubarray.java
@@ -0,0 +1,21 @@
+class maxSubarray {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        int right = 0;
+
+        while(right < nums.length){
+            sum += nums[right];
+            max = Math.max(max, sum);
+
+            if(sum < 0){
+                sum = 0;
+            }
+
+            right++;
+        }
+        
+
+        return max;
+    }
+}
